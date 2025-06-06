125bd8b1ebbb6912c24e053c9e4798c5b0312145
Eduardo Medeiros <eduardo.medeiros@hotmail.com>
2025-06-05 22:05:05 -0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from app.model.nlp import extrair_tributos
 from app.schemas import ExtracaoResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Permitir todas as origens (ou especifique as origens confiáveis)
+origins = ["*"]  # Ou algo como ["https://meufrontend.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["OPTIONS","POST"],  # ou especifique ["GET", "POST", etc.]
+    allow_headers=["*"],
+)
+
 
 class TextoEntrada(BaseModel):
     texto: str
